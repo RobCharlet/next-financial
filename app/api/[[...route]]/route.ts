f024169c9eb99099c58f6e01ec70ea66215d3142
Robin Charlet <contact@robincharlet.fr>
2024-08-14 11:41:15 +0200

    Example to organise Hono routes to a larger application.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,56 +3,61 @@ import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { z } from 'zod'
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get(
-  '/hello',
-  // Handle Clerk authorization, prevents from redirecting.
-  clerkMiddleware()
-  , (c) => {
+// app.get(
+//   '/hello',
+//   // Handle Clerk authorization, prevents from redirecting.
+//   clerkMiddleware()
+//   , (c) => {
 
-  const auth = getAuth(c);
+//   const auth = getAuth(c);
 
-  if (!auth?.userId) {
-    return c.json({ error: "Unauthorized" });
-  }
+//   if (!auth?.userId) {
+//     return c.json({ error: "Unauthorized" });
+//   }
   
-  return c.json({
-    message: 'Hello Next.js!',
-  });
-})
+//   return c.json({
+//     message: 'Hello Next.js!',
+//   })
+// })
 
-app.get(
-  '/hello/:test',
-  zValidator("param", z.object({
-    test: z.string()
-  }))
-  , (c) => {
-  const { test } = c.req.valid("param");
+// app.get(
+//   '/hello/:test',
+//   zValidator("param", z.object({
+//     test: z.string()
+//   }))
+//   , (c) => {
+//   const { test } = c.req.valid("param");
 
-  return c.json({
-    message: "Hello World",
-    test: test
-  })
-})
-.post("/create/:postId",
-  zValidator("json", z.object({
-    name: z.string(),
-    userId: z.number(),
-  })),
-  zValidator("param", z.object({
-    postId: z.number(),
-  })),
-  (c) => {
-    const {name, userId} = c.req.valid("json");
-    const {postId} = c.req.valid("param");
+//   return c.json({
+//     message: "Hello World",
+//     test: test
+//   })
+// })
+// .post("/create/:postId",
+//   zValidator("json", z.object({
+//     name: z.string(),
+//     userId: z.number(),
+//   })),
+//   zValidator("param", z.object({
+//     postId: z.number(),
+//   })),
+//   (c) => {
+//     const {name, userId} = c.req.valid("json")
+//     const {postId} = c.req.valid("param")
 
-    return c.json({});
-  }
-)
+//     return c.json({})
+//   }
+// )
+
+app.route('/authors', authors);
+app.route('/books', books);
 
 export const GET = handle(app)
 export const POST = handle(app)
