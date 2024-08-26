3eebf3ccb2fc2ec55503d6bf56e8db4614f84385
Robin Charlet <contact@robincharlet.fr>
2024-08-26 12:13:07 +0200

    Create API GET accounts by id

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -30,6 +30,45 @@ const app = new Hono()
 
       return c.json({data})
     })
+  .get("/:id",
+    zValidator("param", z.object({
+      id: z.string().optional(),
+    })),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c)
+      const { id } = c.req.valid("param")
+      console.log(c)
+
+      if (!id) {
+        return c.json({error: "Bad request"}, 400)
+      }
+
+      if (!auth?.userId) {
+        return c.json({error: "Unauthorized"}, 401)
+      }
+
+      const [data] = await db
+      // .select automaticaly returns data.
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(
+        and(
+          eq(accounts.userId, auth.userId),
+          eq(accounts.id, id)
+        )
+      )
+
+      if (!data) {
+        return c.json({error: "Not found"}, 404)
+      }
+
+      return c.json({data})
+    }
+  )
   .post("/",
     clerkMiddleware(),
     // Get the schema created with createInsertSchema.
