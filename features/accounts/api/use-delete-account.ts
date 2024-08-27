39d3167adc3f4971f1cfc2cfaceb96726f456cfa
Robin Charlet <contact@robincharlet.fr>
2024-08-27 14:04:29 +0200

    Based on the provided code and Git output, I'd suggest the following one-liner commit message:
    `Added custom hook `useDeleAccount` for deleting an account.`

---
+++ features/accounts/api/use-delete-account.ts
@@ -0,0 +1,44 @@
+import { toast } from "sonner";
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
+
+// Define the response type of the API endpoint
+type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$delete"]>
+
+// Custom hook for editing an account
+export const useDeleAccount = (id?: string) => {
+  // Get the query client instance to manage query cache
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<
+    ResponseType,
+    Error
+  >({
+    // Function that performs the mutation (API request)
+    mutationFn: async () => {
+      // Make a POST request to the API with the provided JSON data
+      const response = await client.api.accounts[":id"]["$delete"]({
+        param: {id}
+      })
+
+      // Return the JSON response from the server
+      return await response.json()
+    },
+    // Callback function for when the mutation is successful
+    onSuccess: () => {
+      toast.success("Account deleted")
+      // Invalidate 'account' query
+      queryClient.invalidateQueries({ queryKey: ["account", {id}]})
+      // Invalidate the 'accounts' query to refetch and update the data
+      // on success. Data will be automatically updated on account page.
+      queryClient.invalidateQueries({ queryKey: ["accounts"] })
+    },
+    onError: () => {
+      toast.error("Failed to delete account")
+    }
+  })
+
+  // Return the mutation object to be used in components
+  return mutation
+}
