1d01e283d2ca8a68f2f2dbd9298584ebd2ff3790
Robin Charlet <contact@robincharlet.fr>
2024-09-04 11:22:38 +0200

    feat(ui): add chart custom tooltip

--- components/area-variant.tsx
+++ components/area-variant.tsx
@@ -1,5 +1,13 @@
 import { format } from "date-fns"
-import { Area, AreaChart, CartesianGrid, ResponsiveContainer, XAxis } from "recharts"
+import { 
+  Area, 
+  AreaChart, 
+  CartesianGrid, 
+  ResponsiveContainer, 
+  XAxis, 
+  Tooltip 
+} from "recharts"
+import { CustomTooltip } from "./custom-tooltip"
 
 type Props = {
   data: {
@@ -16,11 +24,11 @@ export const AreaVariant = ({data} : Props) => {
       <AreaChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <defs>
-          <linearGradient id="income" x1="0" y1="0">
+          <linearGradient id="income" x1="0" y1="0" x2="0" y2="1">
             <stop offset="2%" stopColor="#3d82f6" stopOpacity={0.8} />
             <stop offset="98%" stopColor="#3d82f6" stopOpacity={0} />
           </linearGradient>
-          <linearGradient id="expenses" x1="0" y1="0">
+          <linearGradient id="expenses" x1="0" y1="0" x2="0" y2="1">
             <stop offset="2%" stopColor="#f43f5e" stopOpacity={0.8} />
             <stop offset="98%" stopColor="#f43f5e" stopOpacity={0} />
           </linearGradient>
@@ -33,6 +41,10 @@ export const AreaVariant = ({data} : Props) => {
           style={{ fontSize: "12px" }}
           tickMargin={16}
         />
+        <Tooltip content={
+          //https://recharts.org/en-US/examples/CustomContentOfTooltip
+          <CustomTooltip />
+        } />
         <Area 
           type="monotone"
           dataKey="income"
